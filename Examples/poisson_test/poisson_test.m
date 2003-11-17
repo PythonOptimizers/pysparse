0487ff3d69bccd9e9c59d24abd826c52eb329de8
Roman Geus <geus@users.sourceforge.net>
2003-11-17 15:38:11 +0000

    added iteration over problem sizes

--- Examples/poisson_test/poisson_test.m
+++ Examples/poisson_test/poisson_test.m
@@ -1,8 +1,16 @@
-n = 500;
-tic
-L = poisson2d_kron(n);
-toc
-tic
-[x,flag,relres,iter] = pcg(L, ones(n*n,1), 1e-12, 2000, [], [], ...
-			   zeros(n*n,1));
-toc
+% $Id$
+%
+
+for n = [100, 300, 500]
+
+   n
+   tic
+   L = poisson2d_kron(n);
+   toc
+   b = ones(n*n,1);
+   tic
+   [x,flag,relres,iter] = pcg(L, b, 1e-8, 2000, [], [], zeros(n*n,1));
+   toc
+   iter, relres, norm(b - L*x)
+
+end
