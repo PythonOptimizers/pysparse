189b81d6a79d56c507efb603d956155dcdc3d97b
Nathaniel C. Domingo <gilba@users.sourceforge.net>
2004-02-14 09:37:57 +0000

    uploaded umfpack enhancement

--- setup.py
+++ setup.py
@@ -5,9 +5,20 @@ import glob
 import os, socket
 
 # default settings
-library_dirs_list = []
+library_dirs_list= []
+libraries_list = ['lapack', 'blas', 'g2c']
 superlu_defs = [('USE_VENDOR_BLAS',1)]
 f77_defs = []
+
+umfpack_defs = [('DINT', 1), ('NBLAS', 1)] # most basic configuration, no BLAS
+umfpack_libraries = ['m']
+umfpack_include_dirs = ['amd', 'umfpack']
+umfpack_library_dirs = []
+
+#umfpack_defs = [('DINT', 1), ('CBLAS', 1)] # with atlas c-blas (http://math-atlas.sourceforge.net)
+#umfpack_libraries = ['atlas', 'cblas', 'm']
+#umfpack_include_dirs = ['amd', 'umfpack'] # you may need to set this to find the atlas
+#umfpack_library_dirs = []
 
 hostname = socket.gethostname()
 if hostname in ['bree', 'brokoli', 'givens2', 'givens4', 'nedelec', 'gondor']:
@@ -16,7 +27,7 @@ if hostname in ['bree', 'brokoli', 'givens2', 'givens4', 'nedelec', 'gondor']:
 elif hostname == 'maxwell':
     # AMD Opteron 'x86-64' architecture using ACML
     libraries_list = ['acml', 'g2c']
-    library_dirs_list = ['/opt/acml/gnu64/lib']
+    library_dirs_list = ['/opt/acml/gnu64']
 elif hostname == 'sysiphus':
     # Linux RedHat 7.3 2.4.18-10 i686 with atlas Lapack routines
     library_dirs_list= ['/hg/u/vasseur/Linux/lib/atlas']
@@ -123,6 +134,106 @@ ext_modules = [Extension('spmatrix', ['Src/spmatrixmodule.c']),
                          library_dirs=library_dirs_list,
                          libraries=libraries_list,
                          define_macros=f77_defs),
+               Extension('umfpack', sources=[os.path.join('Src', 'umfpackmodule.c'),
+                                     'amd/amd_aat.c',
+                                     'amd/amd_1.c',
+                                     'amd/amd_2.c',
+                                     'amd/amd_dump.c',
+                                     'amd/amd_postorder.c',
+                                     'amd/amd_post_tree.c',
+                                     'amd/amd_defaults.c',
+                                     'amd/amd_order.c',
+                                     'amd/amd_control.c',
+                                     'amd/amd_info.c',
+                                     'amd/amd_valid.c',
+                                     'umfpack/umf_analyze.c',
+                                     'umfpack/umf_apply_order.c',
+                                     'umfpack/umf_colamd.c',
+                                     'umfpack/umf_free.c',
+                                     'umfpack/umf_fsize.c',
+                                     'umfpack/umf_is_permutation.c',
+                                     'umfpack/umf_malloc.c',
+                                     'umfpack/umf_realloc.c',
+                                     'umfpack/umf_report_perm.c',
+                                     'umfpack/umf_singletons.c',
+                                     'umfpack/umfpack_timer.c',
+                                     'umfpack/umfpack_tictoc.c',
+                                     'umfpack/umf_lhsolve.c',
+                                     'umfpack/umf_uhsolve.c',
+                                     'umfpack/umf_triplet_map_nox.c',
+                                     'umfpack/umf_triplet_nomap_x.c',
+                                     'umfpack/umf_triplet_nomap_nox.c',
+                                     'umfpack/umf_triplet_map_x.c',
+                                     'umfpack/umf_assemble_fixq.c',
+                                     'umfpack/umf_assemble.c',
+                                     'umfpack/umf_blas3_update.c',
+                                     'umfpack/umf_build_tuples.c',
+                                     'umfpack/umf_create_element.c',
+                                     'umfpack/umf_dump.c',
+                                     'umfpack/umf_extend_front.c',
+                                     'umfpack/umf_garbage_collection.c',
+                                     'umfpack/umf_get_memory.c',
+                                     'umfpack/umf_init_front.c',
+                                     'umfpack/umf_kernel.c',
+                                     'umfpack/umf_kernel_init.c',
+                                     'umfpack/umf_kernel_wrapup.c',
+                                     'umfpack/umf_local_search.c',
+                                     'umfpack/umf_lsolve.c',
+                                     'umfpack/umf_ltsolve.c',
+                                     'umfpack/umf_mem_alloc_element.c',
+                                     'umfpack/umf_mem_alloc_head_block.c',
+                                     'umfpack/umf_mem_alloc_tail_block.c',
+                                     'umfpack/umf_mem_free_tail_block.c',
+                                     'umfpack/umf_mem_init_memoryspace.c',
+                                     'umfpack/umf_report_vector.c',
+                                     'umfpack/umf_row_search.c',
+                                     'umfpack/umf_scale_column.c',
+                                     'umfpack/umf_set_stats.c',
+                                     'umfpack/umf_solve.c',
+                                     'umfpack/umf_symbolic_usage.c',
+                                     'umfpack/umf_transpose.c',
+                                     'umfpack/umf_tuple_lengths.c',
+                                     'umfpack/umf_usolve.c',
+                                     'umfpack/umf_utsolve.c',
+                                     'umfpack/umf_valid_numeric.c',
+                                     'umfpack/umf_valid_symbolic.c',
+                                     'umfpack/umf_grow_front.c',
+                                     'umfpack/umf_start_front.c',
+                                     'umfpack/umf_2by2.c',
+                                     'umfpack/umf_store_lu.c',
+                                     'umfpack/umf_scale.c',
+                                     'umfpack/umfpack_wsolve.c',
+                                     'umfpack/umfpack_col_to_triplet.c',
+                                     'umfpack/umfpack_defaults.c',
+                                     'umfpack/umfpack_free_numeric.c',
+                                     'umfpack/umfpack_free_symbolic.c',
+                                     'umfpack/umfpack_get_numeric.c',
+                                     'umfpack/umfpack_get_lunz.c',
+                                     'umfpack/umfpack_get_symbolic.c',
+                                     'umfpack/umfpack_numeric.c',
+                                     'umfpack/umfpack_qsymbolic.c',
+                                     'umfpack/umfpack_report_control.c',
+                                     'umfpack/umfpack_report_info.c',
+                                     'umfpack/umfpack_report_matrix.c',
+                                     'umfpack/umfpack_report_numeric.c',
+                                     'umfpack/umfpack_report_perm.c',
+                                     'umfpack/umfpack_report_status.c',
+                                     'umfpack/umfpack_report_symbolic.c',
+                                     'umfpack/umfpack_report_triplet.c',
+                                     'umfpack/umfpack_report_vector.c',
+                                     'umfpack/umfpack_solve.c',
+                                     'umfpack/umfpack_symbolic.c',
+                                     'umfpack/umfpack_transpose.c',
+                                     'umfpack/umfpack_triplet_to_col.c',
+                                     'umfpack/umfpack_scale.c',
+                                     'umfpack/umfpack_load_numeric.c',
+                                     'umfpack/umfpack_save_numeric.c',
+                                     'umfpack/umfpack_load_symbolic.c',
+                                     'umfpack/umfpack_save_symbolic.c'],
+                         define_macros=umfpack_defs, include_dirs=umfpack_include_dirs,
+                         libraries=umfpack_libraries, library_dirs=umfpack_library_dirs)
+               
+                                     
                ]
 
 execfile(os.path.join('Lib', 'pysparse_version.py'))
