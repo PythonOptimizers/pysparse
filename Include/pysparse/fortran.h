87eb50c5ef48a78aa2bdc40dd32954e19b49d8e4
Roman Geus <hamsel@users.sourceforge.net>
2010-02-07 16:05:51 +0000

    Implemented conversion of Fortran symbols to upper case: F77UPPERCASE (default naming scheme for Intel Fortran on Win32)

--- Include/pysparse/fortran.h
+++ Include/pysparse/fortran.h
@@ -1,8 +1,24 @@
 #ifndef FORTRAN_H
 #define FORTRAN_H
 
-#ifdef NOF77UNDERSCORE
+#if defined(NOF77UNDERSCORE)
 #define F77(s) s
+#elif defined (F77UPPERCASE)
+#define F77(s) s
+#define dgemv DGEMV
+#define dgetrf DGETRF
+#define dgetrs DGETRS
+#define dscal DSCAL
+#define ilaenv ILAENV
+#define dsyev DSYEV
+#define dgemm DGEMM
+#define daxpy DAXPY
+#define dnrm2 DNRM2
+#define dcopy DCOPY
+#define ddot DDOT
+#define dlaset DLASET
+#define dlacpy DLACPY
+#define dlarnv DLARNV
 #else
 #define F77(s) s##_
 #endif
