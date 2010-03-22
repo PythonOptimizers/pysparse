ef18e352b9faba8cc48702c42201e8da2340883a
Dominique Orban <dominique.orban@gmail.com>
2010-03-22 12:35:39 +0000

    Added default versions of AMD, UMFPACK and SuperLU.

--- pysparse/direct/setup.py
+++ pysparse/direct/setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+def getoption(config, section, option):
+    try:
+        val = config.get(section,option)
+    except:
+        val = None
+    return val
+
+
 def configuration(parent_package='',top_path=None):
     import numpy
+    import fnmatch
     import os
+    import sys
     import ConfigParser
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import get_info, NotFoundError
@@ -10,37 +20,92 @@ def configuration(parent_package='',top_path=None):
     # Read relevant PySparse-specific configuration options.
     pysparse_config = ConfigParser.SafeConfigParser()
     pysparse_config.read(os.path.join(top_path, 'site.cfg'))
-    umfpack_libdir = pysparse_config.get('UMFPACK', 'umfpack_libdir')
-    umfpack_include = pysparse_config.get('UMFPACK', 'umfpack_include')
-    superlu_libdir = pysparse_config.get('SuperLU', 'superlu_libdir')
-    superlu_include = pysparse_config.get('SuperLU', 'superlu_include')
-    amd_libdir = pysparse_config.get('AMD', 'amd_libdir')
-    amd_include = pysparse_config.get('AMD', 'amd_include')
+
+    umfpack_libdir = getoption(pysparse_config, 'UMFPACK', 'umfpack_libdir')
+    umfpack_include = getoption(pysparse_config, 'UMFPACK', 'umfpack_include')
+    superlu_libdir = getoption(pysparse_config, 'SuperLU', 'superlu_libdir')
+    superlu_include = getoption(pysparse_config, 'SuperLU', 'superlu_include')
+    amd_libdir = getoption(pysparse_config, 'AMD', 'amd_libdir')
+    amd_include = getoption(pysparse_config, 'AMD', 'amd_include')
 
     config = Configuration('direct', parent_package, top_path)
+    cwd = config.local_path
 
     # Get BLAS info from site.cfg
     blas_info = get_info('blas_opt',0)
     if not blas_info:
         print 'No blas info found'
 
-    umfpack_src= ['umfpackmodule.c']
+    # If UMFPACK or AMD library was not specified, use default.
+    umfpack_lib = ['umfpack']
+    umfpack_src = []
+    amd_lib = ['amd']
+    amd_src = []
+    if umfpack_libdir is None or amd_libdir is None:
+        print 'Using default UMFPACK and AMD'
+        print ' If you do not like this, edit the [UMFPACK] and [AMD]'
+        print ' sections of site.cfg'
+        umfpack_lib = []
+        umfpack_libdir = []
+        umfpack_srcs = fnmatch.filter(os.listdir(os.path.join(cwd,
+                                                              'umfpack','src')),
+                                      '*.c')
+        umfpack_src = [os.path.join('umfpack','src',f) for f in umfpack_srcs]
+        umfpack_include = [os.path.join('umfpack','include')]
+        amd_lib = []
+        amd_libdir = []
+        amd_srcs = fnmatch.filter(os.listdir(os.path.join(cwd,'amd','src')),
+                                             '*.c')
+        amd_src = [os.path.join('amd','src',f) for f in amd_srcs]
+        amd_include = [os.path.join('amd','include')]
+    else:
+        amd_libdir = [amd_libdir]
+        amd_include = [amd_include]
+        umfpack_libdir = [umfpack_libdir]
+        umfpack_include = [umfpack_include]
+
+    # Build UMFPACK extension.
+    umfpack_src.append(os.path.join('src','umfpackmodule.c'))
     config.add_extension(
         name='umfpack',
-        sources=[os.path.join('src',name) for name in umfpack_src],
-        libraries=['amd', 'umfpack'],
-        library_dirs=[amd_libdir] + [umfpack_libdir],
-        include_dirs=['src'] + [umfpack_include] + [amd_include],
+        sources=umfpack_src + amd_src,
+        libraries=amd_lib + umfpack_lib,
+        library_dirs=amd_libdir + umfpack_libdir,
+        include_dirs=['src'] + umfpack_include + amd_include,
         extra_info=blas_info,
         )
 
-    superlu_src= ['superlu3module.c']
+    # If SuperLU library was not specified, use default.
+    superlu_lib = ['superlu']
+    superlu_src = []
+    if superlu_libdir is None:
+        print 'Using default SuperLU.'
+        print ' If you do not like this, edit the [SuperLU] section of site.cfg'
+        superlu_lib = []
+        superlu_libdir = []
+        superlu_srcs = fnmatch.filter(os.listdir(os.path.join(cwd,
+                                                              'superlu','src')),
+                                      '*.c')
+        superlu_src = [os.path.join('superlu','src',f) for f in superlu_srcs]
+        superlu_src += [os.path.join('src','superlumodule.c')]
+        superlu_include = [os.path.join('superlu','include')]
+    else:
+        superlu_libdir = [superlu_libdir]
+        superlu_include = [superlu_include]
+        superlu_src= [os.path.join('src','superlu3module.c')]
+
+    # Build SuperLU extension.
+    superlu_defs = [('USE_VENDOR_BLAS',1)]
+    if sys.platform == 'win32':
+            superlu_defs += [('NO_TIMER', 1)]
+
     config.add_extension(
         name='superlu',
-        sources=[os.path.join('src',name) for name in superlu_src],
-        libraries=['superlu'],
-        library_dirs=[superlu_libdir],
-        include_dirs=['src'] + [superlu_include],
+        sources=superlu_src,
+        define_macros=superlu_defs,
+        libraries=superlu_lib,
+        library_dirs=superlu_libdir,
+        include_dirs=['src'] + superlu_include,
         extra_info=blas_info,
         )
 
